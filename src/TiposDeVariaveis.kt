65703d5f1a66f56e7f3c4b416f85a1b984032fe6
Odair <odavita32@gmail.com>
2025-01-26 13:28:28 -0300

    1 - Função Print e Println 2 - Declarar uma variável 3 - Tipos de variáveis

---
+++ src/TiposDeVariaveis.kt
@@ -0,0 +1,14 @@
+fun main(){
+    /*
+    * String - textos
+    * byte - Números inteiros
+    * int - Números inteiros
+    * long - Números inteiros
+    * float - Números decimais
+    * double - Números decimais
+    * boolean - Valores booleanos
+    * https://developer.android.com/codelabs/basic-android-kotlin-compose-variables#2
+    * */
+
+
+}
